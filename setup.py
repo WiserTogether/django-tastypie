fa0fc88bb107f9067e9d42320a592810c68ba7c7
Erik LaBianca <erik.labianca@gmail.com>
2013-02-10 14:04:22 -0500

    bump version again to make sure everybody gets the good stuff

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='django-tastypie',
-    version='0.9.11.1',
+    version='0.9.11.2',
     description='A flexible & capable API layer for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
